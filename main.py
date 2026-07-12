d55be605562b4ca67939382a651330a4ed3a6181
SurfyGalaxy <ashton3141@gmail.com>
2026-07-12 21:30:13 +1000

    figure out how to actually do diffs

--- main.py
+++ main.py
@@ -5,65 +5,40 @@ def make_diff(file1, file2):
     with open(file2) as f:
         data_new = f.read()
     
-    original_chars = []
-    new_chars = []
-    for char in data_original:
-        original_chars.append(char)
-    for char in data_new:
-        new_chars.append(char)
-    
-    len_original = len(original_chars)
-    len_new = len(new_chars)
-    if len_original >= len_new:
-        max_len = len_original
+    original_lines = data_original.split("\n")
+    new_lines = data_new.split("\n")
+
+    if len(original_lines) < len(new_lines):
+        shared_length = len(original_lines)
+        new_longer = True
     else:
-        max_len = len_new
+        shared_length = len(new_lines)
+        new_longer = False
     
     index = 0
-
-    diff = []
-    original_line = True
-    original_file = True
-    new_line = True
-    new_file = True
-    while index < max_len:
-        if index < len(original_chars):
-            original_char = original_chars[index]
-        else:
-            original_char = "<EOF>"
-            original_file = False
-        if index < len(new_chars):
-            new_char = new_chars[index]
-        else:
-            new_char = "<EOF>"
-            new_file = False
-        
-        if original_char:
-            if original_char == "\n":
-                original_line = False
-        else:
-            if original_char not in ("\n", " "):
-                original_line = True
-
-        if new_line:
-            if new_char == "\n":
-                new_line = False
-        else:
-            if new_char not in ("\n", " "):
-                new_line = True
-        
-        pair = (original_char, new_char)
-        missing_part = False
-        for part in pair:
-            if not missing_part:
-                if part in ("<EOF>", "<ADD>"):
-                    missing_part = True
-                else:
-                    missing_part = False
-        if missing_part:
-            diff.append(("EOF", pair))
-        else:
-            diff.append(pair)
+    while index < shared_length:
+        original = original_lines[index]
+        new = new_lines[index]
         index += 1
-    print(diff)
-make_diff("yes.txt", "no.txt")
+    
+    if new_longer:
+        length = len(new_lines)
+        difference = length - len(original_lines)
+        while difference > 0:
+            original_lines.append(True)
+            difference -= 1
+    else:
+        length = len(original_lines)
+        difference = length - len(new_lines)
+        while difference > 0:
+            new_lines.append(True)
+            difference -= 1
+    
+    index = 0
+    while index < length:
+        new_lines[index]
+        original_lines[index]
+    
+        index += 1
+
+make_diff("no.txt", "yes.txt")
